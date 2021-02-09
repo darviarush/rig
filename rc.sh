a6f7b7b19d5fb2368431f4541cd840a2cfb693b3
darviarush <halfdroy@gmail.com>
2021-02-09 14:50:50 +0300

    master fn

--- rc.sh
+++ rc.sh
@@ -87,3 +87,10 @@ alias install_pip='curl https://bootstrap.pypa.io/get-pip.py > /tmp/get-pip.py &
 github() {
     git clone git@github.com:darviarush/$1.git
 }
+
+# mk snippet - выполняет скрипт из каталога snippet
+mk() {
+    x=$1
+    shift
+    . $RIG_RC/snippet/$x.sh $*
+}
