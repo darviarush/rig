17eaef121fef80901d2ee343f493087bb3412e79
Yaroslav O. Kosmina <darviarush@mail.ru>
2021-11-24 14:11:14 +0300

    master fn

--- rc.sh
+++ rc.sh
@@ -163,7 +163,7 @@ release() {
 
 # version - версия из README.md текущего проекта
 version() {
-    perl -n -e 'print("$1\n"), exit if /^#+[ \t]+VERSION\s*(\S+)/' README.md
+    perl -e '$_=join "", <>; print("$1\n") if /^#+[ \t]+VERSION\s+(\S+)/m' README.md
 }
 
 # github name - клонировать с github мой проект
