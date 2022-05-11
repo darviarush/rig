1ab456e988a5a28fc88b298eef127c310c3cfd21
Yaroslav O. Kosmina <darviarush@mail.ru>
2022-05-11 11:19:03 +0300

    6 Ветка!

--- rc.sh
+++ rc.sh
@@ -45,6 +45,7 @@ run() {
     if [ "$c" != 0 ]; then echo "Завершение команды: $c. Выходим"; fi
 }
 
+# git_diff - при изменениях в репозитории предлагает пользователю варианты действий с ними
 git_diff() {
     m=`git status -s`
     if [ "$m" != "" ]; then
@@ -69,7 +70,7 @@ alias desc='git config branch.`branch`.description'
 
 # new branch - создаёт ветку
 new() {
-    if "`git_diff`" == 1; then return; fi
+    if ! git_diff; then return; fi
     run git checkout master
     run git pull origin master
     local b=`echo "$1" | awk '{print $1}'`
@@ -112,7 +113,7 @@ bdiff() {
 commit() {
     if [ "`sta`" != "" ]; then
         sta
-        run "git add ."
+        run git add .
         run "git commit -am \"`branch` ${1:-`desc`}\""
     fi
 }
