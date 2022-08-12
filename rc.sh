252fbe4a750141e5da215ddd6e5c74618493af95
Yaroslav O. Kosmina <darviarush@mail.ru>
2022-08-12 10:40:43 +0300

    master fn

--- rc.sh
+++ rc.sh
@@ -53,8 +53,7 @@ git_diff() {
     local m=`git status -s`
     if [ "$m" != "" ]; then
         git status -s
-        PS3="Ваш выбор:"
-        select i in Комитим Ресетим stash Пропускаем Отмена
+        PS3="Ваш выбор:" select i in Комитим Ресетим stash Пропускаем Отмена
         do
             case $i in
                 Комитим) read -p "Введите комментарий: " a; run git add .; run git commit -am "$a";;
