981a24dcede79fa14ce2771603ca9cff705931cf
Yaroslav O. Kosmina <darviarush@mail.ru>
2022-12-16 17:19:46 +0300

    master fn

--- snippet/view/Listing.pm
+++ snippet/view/Listing.pm
@@ -3,87 +3,19 @@ package {{pkg}}::Listing;
 
 use common::sense;
 use Aion::View;
-use Astrobook::Common::Head;
-use Astrobook::Libra::Pages::View;
-use Astrobook::Libra::View;
+use Shop::Common::Head;
+use Shop::Product::View;
 
-# Идентификатор книги в томе
-has book_id => (is => "ro", isa => Maybe[Nat], in => "path");
+# Товар
+has product => (is => "ro", isa => Maybe[Model['Shop::Product::View']], in => "path");
 
-# Тело книги
-has book => (is => "ro", isa => Maybe[Str], in => "data", coerce => EmptyStrToUndef);
-
-# Содержание
-has chapters => (is => 'ro', isa => ArrayRef, default => sub {
-	my ($self) = @_;
-	query_ref "SELECT
-		id as book_id,
-		chapter,
-		title
-	FROM book
-	WHERE tom_id=(SELECT tom_id FROM book WHERE id=:book_id)
-	ORDER BY chapter", book_id => $self->book_id;
-});
-
-
-
-# Метод PUT /libra/{book_id}/pages “Редактировать книгу” с авторизацией
-sub put {
-	my ($self) = @_;
-
-	my $book = query_row "SELECT
-		tom_id,
-		user.id as user_id
-	FROM book
-	INNER JOIN author ON author.id=book.author_id
-	LEFT JOIN user ON user.author_id=author.id
-	WHERE id=:book_id", book_id => $self->book_id;
-
-	die Aion::Response->not_found("Книга не найдена.") if !$book;
-	die Aion::Response->forbidden("Доступ запрещён.") if !defined($book->{user_id}) || $book->{user_id} != is_auth();
-
-	# Тут book уже с <PAGE/> или нет?
-	update book => $self->book_id, book => to_book $self->book;
-
-	return {
-		id => $self->book_id,
-	}
-}
 
 # Метод GET /libra/{book_id}/pages “Читать книгу”
 sub get {
 	my ($self) = @_;
 
-	my $book = query_row "SELECT ${\ Astrobook::Libra::View->selectors },
-		book.chapter,
-		book.book
-	FROM book
-	LEFT JOIN author ON author.id=book.author_id
-	WHERE book.id=:book_id",
-		book_id => $self->book_id,
-	;
-
-	return Aion::Response->not_found("Книга не найдена.") if !$book;
-
-	if( !$book->{tom_id} ) {
-		my $book_id = query_scalar "SELECT id FROM book WHERE tom_id=:tom_id ORDER BY chapter LIMIT 1", tom_id => $self->book_id;
-		return Aion::Response->bad_request("У тома нет глав.") if !defined $book_id;
-		return Aion::Response->redirect("/libra/$book_id/pages");
-	}
-
-	my @x = split m!<PAGE/>!, $book->{book};
-
-	# Бъём текст на страницы по размеру
-	my $i = 1;
-	my @pages = map Astrobook::Libra::Pages::View->new(%$book, page => $_, number => $i++),
-		@x;
-
-	my ($next) = grep $_->{chapter} > $book->{chapter}, @{$self->chapters};
-
-	my $next_href = $next? "/libra/$next->{book_id}/pages": "";
-
 	Astrobook::Common::Head->new(
-		title => "Чтение $book->{author_name} «$book->{title}»",
+		title => "Товар ${ \ $self->product->name }",
 		meta => Astrobook::Libra::Pages::View->meta . Astrobook::Libra::View->meta,
 		content => $self->view_listing(pages => \@pages, next_href => $next_href),
 		sidebars => [
