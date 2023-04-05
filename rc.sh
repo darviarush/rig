3651c6552a851832ed072477a57cf65adf56fbf4
Yaroslav O. Kosmina <darviarush@mail.ru>
2023-04-05 12:36:49 +0300

    master fn

--- rc.sh
+++ rc.sh
@@ -535,3 +535,18 @@ alias ga='cmd rc:api:gen-docs'
 
 # task - открывает задачу текущей ветки
 alias task='xdg-open https://restoclub.myjetbrains.com/youtrack/issue/`branch`'
+
+# mkmig - создать миграцию на основе текущего времени и открыть её в phpstorm-е
+mkfeature() {
+    local file=`basename $1`
+    local dir=`dirname $1`
+
+    local path=/home/Project/restoclub-2022/frontend/$dir
+    mkdir -p $path
+    pushd $path
+    mk feature $file
+    popd
+
+    /usr/bin/env phpstorm --line 4 "${path}/$file.feature"
+}
+
