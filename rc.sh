2f725f49a0a9d0467656b17984bce2ffbe45564f
Yaroslav O. Kosmina <darviarush@mail.ru>
2023-04-05 16:37:14 +0300

    master fn

--- rc.sh
+++ rc.sh
@@ -524,7 +524,7 @@ mkfeature() {
     local file=`basename $1`
     local dir=`dirname $1`
 
-    local path=/home/Project/restoclub-2022/tests/symphony/$dir
+    local path=/home/Project/restoclub-2022/tests/symfony/$dir
     mkdir -p $path
     pushd $path
     mk feature $file
