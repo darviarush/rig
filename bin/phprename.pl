d7c096211505f34f98b4629ea3ae405cd2474320
Yaroslav O. Kosmina <dart@cpan.org>
2025-03-21 18:32:05 +0300

    master fn

--- bin/phprename.pl
+++ bin/phprename.pl
@@ -3,7 +3,7 @@
 use common::sense;
 use open qw/:std :utf8/;
 
-use Aion::Fs qw/cat find/;
+use Aion::Fs qw/find replace/;
 
 my ($root) = @ARGV;
 $root //= $ENV{RIG_PHPRENAME_NAMESPACE} // 'App';
@@ -16,12 +16,13 @@ find "src", "*.php", sub {
         my ($existsClass, $class, $existsNamespace, $namespace);		
 		
 		# Класс
-        if(/^class\s+(\w+)/m) {
-            $existsClass = $1;
+        if(/\b(class|enum)\s+(\w+)/m) {
+			my $who = $1;
+            $existsClass = $2;
             $class = $a =~ m!([^/]+)\.php$!? $1: die "?";
             if ($existsClass ne $class) {
 				$renameClass{$existsClass} = $class;
-				s/^class\s+$existsClass\b/class $class/m;
+				s/\b$who\s+$existsClass\b/$who $class/m;
             }
         }
         
@@ -29,17 +30,18 @@ find "src", "*.php", sub {
 		if(/^namespace\s+([\w\\]+)/m) {
 			$existsNamespace = $1;
 			($namespace) = $a =~ m!^src(/.*)/! or die "!";
-			print "$namespace\n";
 			$namespace =~ s!/!\\!g;
 			$namespace = $root . $namespace;
 			if ($existsNamespace ne $namespace) {
-				s/^namespace\s+$existsNamespace\b/namespace $namespace/m;
+				s/^namespace\s+${\quotemeta $existsNamespace}\b/namespace $namespace/m;
             }
 		}
 
 		$renameNamespace{"$existsNamespace\\$existsClass"} = "$namespace\\$class" if $class && $namespace && $existsNamespace ne $namespace;
     };
 0};
+
+use DDP; p %renameClass; p %renameNamespace;
 
 # Второй проход для переименования неймспейсов
 find "src", "*.php", sub {
