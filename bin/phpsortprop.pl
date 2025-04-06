b4fe8285a913d4309655cd0a73d84efd827dcaf2
Yaroslav O. Kosmina <dart@cpan.org>
2025-04-07 00:26:01 +0300

    master fn

--- bin/phpsortprop.pl
+++ bin/phpsortprop.pl
@@ -5,6 +5,21 @@ use Aion::Fs;
 
 replace {
 	
-	s///g;
+	my @mod;
+	push @mod, $1 while m!^use[ \t]+(\S+);!gm;
+	
+	s/\bnew\s+(\w+)\s*\([ \t]*\n(.*\n)[ \t]*\)/ replace_param(\@mod, $1, $2) /ge;
 }
 find \@ARGV, '*.php';
+
+
+
+sub replace_param {
+	my ($mod, $class, $params) = @_;
+
+	my @params = split /,[ \t]*\n/, $params;
+	
+	
+	
+	join "", map "$_,\n", @params
+}
