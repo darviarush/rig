7cb1b625710dc5c7ddb978e136b3a584ebd271e9
Yaroslav O. Kosmina <dart@cpan.org>
2025-12-12 16:43:04 +0300

    master fn

--- rc.sh
+++ rc.sh
@@ -258,7 +258,7 @@ upd() {
 # push [comment] - делает комит текущей ветки
 push() {
     local branch
-    if [ -x "act" ]; then ./act meta || return 1; fi
+    #if [ -x "act" ]; then ./act meta || return 1; fi
     branch=`branch`
     if [ "$1" == 1 ]; then commit "`desc`"; else commit "$1"; fi || return $?
     run "git pull origin $branch --no-edit || git merge --no-ff --no-edit origin/$branch" || return $?
